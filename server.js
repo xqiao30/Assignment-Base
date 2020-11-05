62f493d2a71e0d3e19c5c20c9c64954f8599255d
Alex Leitch <aleitch1@umd.edu>
2020-11-05 15:15:04 -0500

    Stub assignment base for INST377

---
+++ server.js
@@ -0,0 +1,40 @@
+// These are our required libraries to make the server work.
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import express from 'express';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.route('/api')
+  .get((req, res) => {
+    console.log('GET request detected');
+    res.send(`Lab 5 for ${process.env.NAME}`);
+  })
+  .post(async (req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    console.log('data from fetch', json);
+    res.json(json);
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
